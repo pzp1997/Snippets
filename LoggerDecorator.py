60f48aa9d29cad82d7032a96d9b6e97d32458f33
Palmer Paul <pzpaul2002@yahoo.com>
2015-08-11 17:32:00 -0400

    Create LoggerDecorator.py
    Decorator that logs the input and output of a function. Useful for debugging.

---
+++ LoggerDecorator.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python2.7
+
+"""
+Decorator that logs the input and output of a function. Useful for debugging.
+"""
+
+from functools import wraps
+
+__author__ = 'Palmer Paul'
+__version__ = '1.0.0'
+__email__ = 'pzpaul2002@yahoo.com'
+
+
+def logger(func):
+    """Logs the input/output of decorated function"""
+    @wraps(func)
+    def inner(*args, **kwargs):
+        ret_val = func(*args, **kwargs)
+        print "{}({}{}{}) --> {}".format(
+            func.__name__,
+            ', '.join(str(x) for x in args),
+            ', ' if len(args) > 0 and len(kwargs) > 0 else '',
+            ', '.join('{}={}'.format(k, v) for k, v in kwargs.iteritems()),
+            ret_val)
+        return ret_val
+    return inner
+
+
+@logger
+def foo(*args, **kwargs):
+    """Returns True if passed an even number of arguments, False otherwise."""
+    return (len(args) + len(kwargs)) & 1 == 0
+
+if __name__ == '__main__':
+    foo()
+    foo(1, 'a', [[1, 2, 3], [4, 5, 6]])
+    foo(a=1, b='2', c='3')
+    foo('abc', xyz='123')

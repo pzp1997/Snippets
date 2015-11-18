4488f7c6c56c47067637aafbd8efe318b0b8e0bf
Palmer Paul <pzpaul2002@yahoo.com>
2015-11-18 11:08:45 -0500

    Update SmallTicTacToe.py
    16 lines. 539 characters (380 chars w/o whitespace).

--- SmallTicTacToe.py
+++ SmallTicTacToe.py
@@ -1,14 +1,16 @@
-import numpy as np
-b,t=np.zeros((3,3)),1
-check_winners=lambda b,p:(any(all(r==p)for r in b)or any(all(b[:,c]==p)for c in range(b.shape[1]))or all(b[i,i]==p for i in range(min(b.shape)))or all(b[i,min(b.shape)-i-1]==p for i in range(min(b.shape))))
+import numpy
+s,b,t=3,numpy.zeros((3,3)),1
 while True:
-    print '\n'.join(' '.join(list(' XO')[int(i)]for i in r)for r in b)
+    print '\n'.join(' '.join(' XO'[int(i)]for i in r)for r in b)
     i=''
-    while not i in list('123456789'):i=raw_input('Cell (1-9): ')
-    c=divmod(int(i)-1,min(b.shape))
+    while i not in list('123456789'):i=raw_input("Cell (1-9): ")
+    c=divmod(int(i)-1,s)
     if b[c]==0:
         b[c]=t
-        if check_winners(b,t):
-            print "{} wins!".format(('X','O')[t-1])
+        if any(all(r==t)for r in b)or any(all(b[:,c]==t)for c in range(s))or all(b[i,i]==t for i in range(s))or all(b[i,s-i-1]==t for i in range(s)):
+            print 'XO'[t-1] + " wins!"
+            break
+        if (b!=0).all():
+            print "Tie!"
             break
         t=t%2+1

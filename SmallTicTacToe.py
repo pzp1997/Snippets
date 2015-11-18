4270e2917a2d01fff10185f9993567b820d605fd
Palmer Paul <pzpaul2002@yahoo.com>
2015-11-18 11:23:51 -0500

    Update SmallTicTacToe.py
    added 11 characters. now can be played with any board size (i.e. 4x4, 5x5).

--- SmallTicTacToe.py
+++ SmallTicTacToe.py
@@ -3,12 +3,12 @@ s,b,t=3,numpy.zeros((3,3)),1
 while True:
     print '\n'.join(' '.join(' XO'[int(i)]for i in r)for r in b)
     i=''
-    while i not in list('123456789'):i=raw_input("Cell (1-9): ")
+    while i not in map(str,range(1,s**2+1)):i=raw_input("Cell (1-%s): "%s**2)
     c=divmod(int(i)-1,s)
     if b[c]==0:
         b[c]=t
         if any(all(r==t)for r in b)or any(all(b[:,c]==t)for c in range(s))or all(b[i,i]==t for i in range(s))or all(b[i,s-i-1]==t for i in range(s)):
-            print 'XO'[t-1] + " wins!"
+            print 'XO'[t-1]+" wins!"
             break
         if (b!=0).all():
             print "Tie!"
